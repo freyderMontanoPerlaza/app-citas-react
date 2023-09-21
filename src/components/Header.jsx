f806d5e0d296ff655ba03c57dbb7257b56119e12
freyder montano <143907744+freyderMontanoPerlaza@users.noreply.github.com>
2023-09-21 11:54:48 -0300

    Aprendiendo React creando una aplicacion de citas con Tailwind CSS,

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold mt-2'>Header</h1>
+        </div>
+    )
+}
+
+export default Header
